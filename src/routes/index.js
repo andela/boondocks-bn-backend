159f931468b226e8c023860efaae9e56ad372b7e
shaphan <shaphan.nzabonimana@andela.com>
2019-11-05 12:48:06 +0200

    chore(api-docs): setup api docs using swagger - creating API specs file, swagger.json - swagger ui integration

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,9 @@
 const router = require("express").Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../docs/swagger.json');
+
+router.use('/api/docs', swaggerUi.serve);
+router.get('/api/docs', swaggerUi.setup(swaggerDocument));
 
 router.use("/api", require("./api"));
 
